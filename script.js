9e09d9b61a5938a0ea927eda1c6be34fa4792c4a
Esther Su <su18@uwindsor.ca>
2018-12-22 08:34:18 -0500

    First commit

---
+++ script.js
@@ -0,0 +1,16 @@
+var i = 0;
+function buttonClick() {
+    
+    i++;
+    document.getElementById('items').textContent = i;
+}
+
+function image1() {
+    document.getElementById("main-img").src = "watch1.jpg";
+}
+function image2() {
+    document.getElementById("main-img").src = "watch2.jpg";
+}
+function image3() {
+    document.getElementById("main-img").src = "watch3.jpg";
+}
